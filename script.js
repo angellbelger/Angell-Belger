dacf96df53e62e43d68d742db728d9639a44d706
Angell Belger <angellbelger@hotmail.com>
2022-06-17 16:10:22 -0300

    update

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 
- 
+var msgMind = window.document.querySelector('p#mind')
+var msgDark = window.document.querySelector('p#dark')
+
+msgMind.addEventListener('mouseenter', showtxt)
+msgMind.addEventListener('mouseout', hiddentxt)
+msgDark.addEventListener('mouseenter', showtxt)
+msgDark.addEventListener('mouseout', hiddentxt)
+msg
+
+
+function showtxt(){
+    msgMind.innerHTML = 'Mind Palace'
+    msgDark.innerHTML = 'Dark Day'
+}
+
+function hiddentxt(){
+    msgMind.innerHTML = '...'
+    msgDark.innerHTML = '...'
+}
